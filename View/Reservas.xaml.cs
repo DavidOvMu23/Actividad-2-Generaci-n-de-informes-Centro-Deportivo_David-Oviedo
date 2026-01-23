4c13eaf04ff14187a391b74ce8a384cca6dfa181
David Oviedo <david23oviedo@gmail.com>
2026-01-23 12:34:43 +0100

    Agregar archivos de proyecto.

---
+++ View/Reservas.xaml.cs
@@ -0,0 +1,18 @@
+﻿using ModelView;
+using System.Windows;
+
+namespace View
+{
+    /// <summary>
+    /// Lógica de interacción para Reservas.xaml
+    /// </summary>
+    public partial class Reservas : Window
+    {
+        public Reservas()
+        {
+            InitializeComponent();
+            // Asignamos el ViewModel como DataContext para que el XAML pueda enlazar sus propiedades y comandos.
+            DataContext = new ReservasViewModel();
+        }
+    }
+}
